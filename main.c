0c16e1ae4b2d8fba7abcfd3652766c255b6f16be
Onyu <loy7153@handong.ac.kr>
2023-05-22 14:57:54 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main(){
+
+ int a = 3, b = 5;
+ printf("(initial) a=%d, b=%d\n". a. b);
+
+ swap(&a, &b);
+ printf("(swapped) aa=%d, b=%d\n", a, b);
+
+ return 0;
+}
